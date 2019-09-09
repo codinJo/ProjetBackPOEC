d3756f4299c37ab126f4c667237e6b200e03220e
yoannbelin <yoann.belin@icloud.com>
2019-09-09 16:30:42 +0200

    addCover works

---
+++ PDFPOEC/src/main/java/AddCover.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import com.itextpdf.forms.PdfPageFormCopier;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+ 
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+/**
+ *
+ * @author imperath
+ */
+@Named(value = "addCover")
+@SessionScoped
+public class AddCover implements Serializable{
+    public static final String DEST
+            = "./src/main/java/pdfs/add_cover4.pdf";
+    public static final String COVER
+            = "./src/main/java/pdfs/couverture.pdf";
+    public static final String RESOURCE
+            = "./src/main/java/pdfs/test.pdf";
+    
+    public void addCover() throws IOException {
+        System.out.println("youpi ca va dedans !!!");
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new AddCover().manipulatePdf(DEST);
+    }
+     
+    public void manipulatePdf(String dest) throws IOException {
+        PdfDocument pdfDoc = new PdfDocument(new PdfReader(RESOURCE), new PdfWriter(dest));
+        PdfDocument cover = new PdfDocument(new PdfReader(COVER));
+        cover.copyPagesTo(1, 1, pdfDoc, 1, new PdfPageFormCopier());
+        cover.close();
+        pdfDoc.close();
+    }
+}
